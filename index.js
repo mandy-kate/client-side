9444bcd6db870b13083b3813c1d15070ddd745d5
Mandy Baker <email": null>
2016-08-09 11:33:46 +1200

    Updates

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ xhr.get(endpoint, function (err, data) {
 
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
-
+  var data = JSON.parse(data.body)
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: data.body})
+  target.innerHTML = greeting({name: data.name})
 })
 
 //   var target = document.getElementsByTagName('main')[0]
