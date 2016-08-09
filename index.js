3c6f80a3d1fab2236fe3bcd16c12c5abc1a014e0
Mandy Baker <email": null>
2016-08-09 13:39:21 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
@@ -16,6 +16,11 @@ xhr.get(endpoint, function (err, data) {
   target.innerHTML = greeting({name: data[0].name})
 })
 
+
+//   // In case you're curious
+//   console.log(data.body) // FYI: data.body is a string
+//   var data = JSON.parse(data.body)
+//   // Replace 'Space' below with the response
 //   var target = document.getElementsByTagName('main')[0]
-//   target.innerHTML = greeting({name: 'Space'})
+//   target.innerHTML = greeting({name: data[0].name})
 // })
