111f5075eb2378387ff3426a873cc506a139fedb
Mandy Baker <email": null>
2016-08-09 15:43:37 +1200

    client side rendering of longitude from iss

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
-
-  // In case you're curious
   console.log(data.body) // FYI: data.body is a string
   var data = JSON.parse(data.body)
-  // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: data[0].name})
+  target.innerHTML = greeting({name: data.name})
+  target.innerHTML = greeting({longitude: data.longitude})
 })
 
+
+//   // Accessing longitude / latitude by ID
+//   console.log(data.body) // FYI: data.body is a string
+//   var data = JSON.parse(data.body)
+//   // Replace 'Space' below with the response
 //   var target = document.getElementsByTagName('main')[0]
-//   target.innerHTML = greeting({name: 'Space'})
+//   target.innerHTML = greeting({longitude: 25544.longitude})
 // })
