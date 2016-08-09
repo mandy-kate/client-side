7060f553c4c4d3cceb818f15743cd7cc56134a2c
Mandy Baker <email": null>
2016-08-09 16:26:57 +1200

    wrote and passed basic test

--- tests/greeting.js
+++ tests/greeting.js
@@ -1,19 +1,19 @@
-npvar test = require('tape')
+var test = require('tape')
 var Handlebars = require('handlebars')
 var fs = require('fs')
 var path = require('path')
 var cheerio = require('cheerio')
 
 test('greeting view', function(t) {
-  var templateText = fs.readFileSync(path.join(__dirname, 'views/greeting.hbs'), 'utf8')
+  var templateText = fs.readFileSync(path.join(__dirname, '../views/greeting.hbs'), 'utf8')
   var templateFunction = Handlebars.compile( templateText )
 
-  var exampleData = {name: 'MIXMIX'}
+  var exampleData = {name: "iss"}
 
   var renderedTemplate = templateFunction(exampleData)
   var $ = cheerio.load(renderedTemplate)
 
-  t.equal($('h1').text(), "Hello MIXMIX!", 'template uses data in heading')
+  t.equal($('h3').text(), "Hello I'm iss!", 'template uses data in heading')
 
   t.end()
 })
